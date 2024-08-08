9e834715876021ae175325c77476b1a78b51b307
Pushpendra Singh Thakur <pushpendra.singh@myn.global>
2024-08-08 15:56:26 +0530

    first commit on main

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * ReactElement(Object) => HTML(Browser Understands)
+ *
+ * Convert third argument to array if siblings elements
+ */
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+// jsx that exists
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello world from React!"
+// );
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent); // replaced by this, whatever exist inside 'root'
