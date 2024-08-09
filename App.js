5e88f950fa85d4172aa619d73bcd2a4fe4d8bc7d
Pushpendra Singh Thakur <pushpendra.singh@myn.global>
2024-08-09 11:19:42 +0530

    Episode-2 Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /**
  * ReactElement(Object) => HTML(Browser Understands)
  *
@@ -5,7 +7,7 @@
  */
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is Namaste react"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
